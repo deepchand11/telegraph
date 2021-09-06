09448ad8c4bfb63a7fbb40a4f665beaaf5a487a5
deepchand11 <deepchand.pati@oup.com>
2021-09-06 20:09:04 +0530

    added files

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const router = require('./router');
 const meta = require("./content/meta.json");
+
 const app = express();
 const port = 3000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('meta', meta);
+
 
 app.use('*/static', express.static('public'));
 
--- server/router.js
+++ server/router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+let likes = require('../db.json');
+
 
 router.get('/', (req, res) => {
 	res.render("home", {
+		meta: req.app.get('meta'),
+		likes: likes.comments
+	});
+});
+
+router.get('/about', (req, res) => {
+	res.render("about", {
 		meta: req.app.get('meta')
 	});
 });
